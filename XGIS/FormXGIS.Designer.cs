fda01f1c42757db27474748ef4051e300248bedf
1723663554 <joker.lord@foxmail.com>
2019-11-01 14:33:50 +0800

    assignment4

---
+++ XGIS/FormXGIS.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace XGIS
+{
+    partial class FormXGIS
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bFullExtent = new System.Windows.Forms.Button();
+            this.bZoomIn = new System.Windows.Forms.Button();
+            this.bZoomOut = new System.Windows.Forms.Button();
+            this.bMoveUp = new System.Windows.Forms.Button();
+            this.bMoveDown = new System.Windows.Forms.Button();
+            this.bMoveLeft = new System.Windows.Forms.Button();
+            this.bMoveRight = new System.Windows.Forms.Button();
+            this.bOpenShapeFile = new System.Windows.Forms.Button();
+            this.bOpenAttribute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bFullExtent
+            // 
+            this.bFullExtent.Location = new System.Drawing.Point(239, 12);
+            this.bFullExtent.Name = "bFullExtent";
+            this.bFullExtent.Size = new System.Drawing.Size(75, 23);
+            this.bFullExtent.TabIndex = 2;
+            this.bFullExtent.Text = "显示全图";
+            this.bFullExtent.UseVisualStyleBackColor = true;
+            this.bFullExtent.Click += new System.EventHandler(this.BFullExtent_Click);
+            // 
+            // bZoomIn
+            // 
+            this.bZoomIn.Location = new System.Drawing.Point(319, 12);
+            this.bZoomIn.Name = "bZoomIn";
+            this.bZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.bZoomIn.TabIndex = 86;
+            this.bZoomIn.Text = "放大";
+            this.bZoomIn.UseVisualStyleBackColor = true;
+            this.bZoomIn.Click += new System.EventHandler(this.MapExplore);
+            // 
+            // bZoomOut
+            // 
+            this.bZoomOut.Location = new System.Drawing.Point(400, 11);
+            this.bZoomOut.Name = "bZoomOut";
+            this.bZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.bZoomOut.TabIndex = 86;
+            this.bZoomOut.Text = "缩小";
+            this.bZoomOut.UseVisualStyleBackColor = true;
+            this.bZoomOut.Click += new System.EventHandler(this.MapExplore);
+            // 
+            // bMoveUp
+            // 
+            this.bMoveUp.Location = new System.Drawing.Point(481, 11);
+            this.bMoveUp.Name = "bMoveUp";
+            this.bMoveUp.Size = new System.Drawing.Size(75, 23);
+            this.bMoveUp.TabIndex = 86;
+            this.bMoveUp.Text = "上移";
+            this.bMoveUp.UseVisualStyleBackColor = true;
+            this.bMoveUp.Click += new System.EventHandler(this.MapExplore);
+            // 
+            // bMoveDown
+            // 
+            this.bMoveDown.Location = new System.Drawing.Point(562, 12);
+            this.bMoveDown.Name = "bMoveDown";
+            this.bMoveDown.Size = new System.Drawing.Size(75, 23);
+            this.bMoveDown.TabIndex = 86;
+            this.bMoveDown.Text = "下移";
+            this.bMoveDown.UseVisualStyleBackColor = true;
+            this.bMoveDown.Click += new System.EventHandler(this.MapExplore);
+            // 
+            // bMoveLeft
+            // 
+            this.bMoveLeft.Location = new System.Drawing.Point(643, 12);
+            this.bMoveLeft.Name = "bMoveLeft";
+            this.bMoveLeft.Size = new System.Drawing.Size(75, 23);
+            this.bMoveLeft.TabIndex = 86;
+            this.bMoveLeft.Text = "左移";
+            this.bMoveLeft.UseVisualStyleBackColor = true;
+            this.bMoveLeft.Click += new System.EventHandler(this.MapExplore);
+            // 
+            // bMoveRight
+            // 
+            this.bMoveRight.Location = new System.Drawing.Point(724, 12);
+            this.bMoveRight.Name = "bMoveRight";
+            this.bMoveRight.Size = new System.Drawing.Size(75, 23);
+            this.bMoveRight.TabIndex = 86;
+            this.bMoveRight.Text = "右移";
+            this.bMoveRight.UseVisualStyleBackColor = true;
+            this.bMoveRight.Click += new System.EventHandler(this.MapExplore);
+            // 
+            // bOpenShapeFile
+            // 
+            this.bOpenShapeFile.Location = new System.Drawing.Point(33, 12);
+            this.bOpenShapeFile.Name = "bOpenShapeFile";
+            this.bOpenShapeFile.Size = new System.Drawing.Size(200, 23);
+            this.bOpenShapeFile.TabIndex = 87;
+            this.bOpenShapeFile.Text = "打开Shapefile";
+            this.bOpenShapeFile.UseVisualStyleBackColor = true;
+            this.bOpenShapeFile.Click += new System.EventHandler(this.BOpenShapeFile_Click);
+            // 
+            // bOpenAttribute
+            // 
+            this.bOpenAttribute.Location = new System.Drawing.Point(805, 12);
+            this.bOpenAttribute.Name = "bOpenAttribute";
+            this.bOpenAttribute.Size = new System.Drawing.Size(75, 23);
+            this.bOpenAttribute.TabIndex = 88;
+            this.bOpenAttribute.Text = "属性信息";
+            this.bOpenAttribute.UseVisualStyleBackColor = true;
+            this.bOpenAttribute.Click += new System.EventHandler(this.BOpenAttribute_Click);
+            // 
+            // FormXGIS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 450);
+            this.Controls.Add(this.bOpenAttribute);
+            this.Controls.Add(this.bOpenShapeFile);
+            this.Controls.Add(this.bMoveRight);
+            this.Controls.Add(this.bMoveLeft);
+            this.Controls.Add(this.bMoveDown);
+            this.Controls.Add(this.bMoveUp);
+            this.Controls.Add(this.bZoomOut);
+            this.Controls.Add(this.bZoomIn);
+            this.Controls.Add(this.bFullExtent);
+            this.Name = "FormXGIS";
+            this.Text = "FormXGIS2019";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormXGIS_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bFullExtent;
+        private System.Windows.Forms.Button bZoomIn;
+        private System.Windows.Forms.Button bZoomOut;
+        private System.Windows.Forms.Button bMoveUp;
+        private System.Windows.Forms.Button bMoveDown;
+        private System.Windows.Forms.Button bMoveLeft;
+        private System.Windows.Forms.Button bMoveRight;
+        private System.Windows.Forms.Button bOpenShapeFile;
+        private System.Windows.Forms.Button bOpenAttribute;
+    }
+}
+
---
+++ XGIS/FormXGIS.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XGIS
+{
+    public partial class FormXGIS : Form
+    {
+        XView view;
+        XLayer layer;
+        public static bool IsMecator = false; 
+        public FormXGIS()
+        {
+            InitializeComponent();
+            view = new XView(
+                    new XExtent(
+                        new XVertex(0, 0),
+                        new XVertex(1, 1)),
+                    this.ClientRectangle
+                );
+        }
+
+
+        private void FormXGIS_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (layer.FeatureCount() == 0) return;
+            XVertex onevertex = view.ToMapVertex(e.Location);
+            double mindistance = Double.MaxValue;
+            int findid = -1;
+            int index = 0;
+            foreach(XFeature feature in layer.Features)
+            {
+                double distance = feature.Distance(onevertex);
+                if (distance < mindistance)
+                {
+                    mindistance = distance;
+                    findid = index;
+                }
+                index++;
+            }
+            int ScreenDistance = view.ToScreenDistance(mindistance);
+            if (ScreenDistance < 5)
+                MessageBox.Show("找到的Feature的序号是"+ findid);
+        }
+
+        //private int ScreenDistance(Point _P1, Point _P2)
+        //{
+        //    return Math.Abs(_P1.X - _P2.X) + Math.Abs(_P1.Y - _P2.Y);
+        //}
+
+        private void BUpdateMap_Click(object sender, EventArgs e)
+        {
+            ////从文本框中获取新的地图范围
+            //double minx = Double.Parse(tbMinX.Text);
+            //double miny = Double.Parse(tbMinY.Text);
+            //double maxx = Double.Parse(tbMaxX.Text);
+            //double maxy = Double.Parse(tbMaxY.Text);
+            ////更新view
+            //view.Update(
+            //    new XExtent(
+            //        new XVertex(minx, miny), 
+            //        new XVertex(maxx, maxy)), 
+            //    ClientRectangle);
+            ////更新地图
+            //UpdateMap();
+        }
+
+        private void UpdateMap()
+        {
+            Graphics graphics = CreateGraphics();
+            //用黑色填充整个窗口
+            graphics.FillRectangle(new SolidBrush(Color.Black), ClientRectangle);
+            //根据新的view在绘图窗口中画上数组中的每个空间对象
+            layer.Draw(graphics, view);
+            graphics.Dispose();
+        }
+
+        private void MapExplore(object sender, EventArgs e)
+        {
+            XMapActions action = XMapActions.none;
+
+            if ((Button)sender == bZoomIn) action = XMapActions.zoomin;
+            else if ((Button)sender == bZoomOut) action = XMapActions.zoomout;
+            else if ((Button)sender == bMoveUp) action = XMapActions.moveup;
+            else if ((Button)sender == bMoveDown) action = XMapActions.movedown;
+            else if ((Button)sender == bMoveLeft) action = XMapActions.moveleft;
+            else if ((Button)sender == bMoveRight) action = XMapActions.moveright;
+
+            view.UpdateExtent(action);
+            UpdateMap();
+
+        }
+
+        private void BOpenShapeFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Shapefile文件|*.shp";
+            openFileDialog.RestoreDirectory = false;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            XShapeFile sf = new XShapeFile();
+            layer = sf.ReadShapefile(openFileDialog.FileName);
+            layer.DrawAttributeOrNot = false;
+            MessageBox.Show("读入" + layer.FeatureCount() + "个实体");
+            view.UpdateExtent(layer.Extent);
+            UpdateMap();
+        }
+
+        private void BFullExtent_Click(object sender, EventArgs e)
+        {
+            view.UpdateExtent(layer.Extent);
+            UpdateMap();
+        }
+
+        private void BOpenAttribute_Click(object sender, EventArgs e)
+        {
+            FormAttribute form = new FormAttribute(layer);
+            form.Show();
+        }
+    }
+}
